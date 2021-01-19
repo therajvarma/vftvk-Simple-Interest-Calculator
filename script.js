dee35ad363639f4c462dea0f4b0f429676fd8f3d
therajvarma <69708665+therajvarma@users.noreply.github.com>
2021-01-19 08:31:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ var result1=((annee=+selectId));
 var today2=new Date();
 var annee2 =today2.getFullYear();
 var result2=result1+annee2
-result.innerHTML="If you deposit"+"&nbsp;"+(amount)+","+"<br/>"+ "at an interest rate of"+"&nbsp;"+(rateInput)+"."+"<br/>"+
-"You will receive an amount of"+"&nbsp;"+(amount*rateInput*selectId/100)+","+"<br/>"+
+result.innerHTML="If you deposit"+"&nbsp;"+"<mark>(amount)</mark>"+","+"<br/>"+ "at an interest rate of"+"&nbsp;"+"<mark>(rateInput)</mark>"+"."+"<br/>"+
+"You will receive an amount of"+"&nbsp;"+"<mark>(amount*rateInput*selectId/100)</mark>"+","+"<br/>"+
 "in the year"+"&nbsp;"+(result2);
 }
 }
