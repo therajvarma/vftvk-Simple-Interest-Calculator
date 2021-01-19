e667d94c535d7fa3e3815897d8044a230d68d533
therajvarma <69708665+therajvarma@users.noreply.github.com>
2021-01-19 09:14:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,25 +3,18 @@ function rateValue(val){
 }
 function calculate(){
 amount = document.getElementById('amount').value;
-if (amount<=0||amount=""){
-alert("enter a postive number")
-	document.getElementById(amount).focus();
-}
-else{
 rateInput= document.getElementById('rateInput').value;
 selectId=document.getElementById('selectId').value;
 result=document.getElementById('result');
-result=amount*rateInput*selectId/100	
-	
 var today= new Date();
 var annee = today.getFullYear();
 var result1=((annee=+selectId));
 var today2=new Date();
 var annee2 =today2.getFullYear();
 var result2=result1+annee2
-result.innerHTML="If you deposit"+"&nbsp;"+"<mark>(amount)</mark>"+","+"<br/>"+ "at an interest rate of"+"&nbsp;"+"<mark>(rateInput)</mark>"+"."+"<br/>"+
-"You will receive an amount of"+"&nbsp;"+"<mark>(amount*rateInput*selectId/100)</mark>"+","+"<br/>"+
-"in the year"+"&nbsp;"+(result2);
+result.innerHTML="If you deposit"+"&nbsp;"+"<mark>"+(amount)+"</mark>"+","+"<br/>"+ "at an interest rate of"+"&nbsp;"+"<mark>"+(rateInput)+"</mark>"+"."+"<br/>"+
+"You will receive an amount of"+"&nbsp;"+"<mark>"+(amount*rateInput*selectId/1001*rateInput*selectId/100)+"</mark>"+","+"<br/>"+
+"in the year"+"&nbsp;"+"<mark>"+(result2)+"</mark>";
 }
-}
+
 
