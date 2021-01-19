75ef95a64a388801f0db6fd1cc4e26a35003409a
therajvarma <69708665+therajvarma@users.noreply.github.com>
2021-01-19 08:23:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ function rateValue(val){
 }
 function calculate(){
 amount = document.getElementById('amount').value;
+if (amount<=0){
+alert("enter a postive number")
+}
+else{
 rateInput= document.getElementById('rateInput').value;
 selectId=document.getElementById('selectId').value;
 result=document.getElementById('result');
+result=amount*rateInput*selectId/100	
+	
 var today= new Date();
 var annee = today.getFullYear();
 var result1=((annee=+selectId));
@@ -15,5 +21,6 @@ var result2=result1+annee2
 result.innerHTML="If you deposit"+"&nbsp;"+(amount)+","+"<br/>"+ "at an interest rate of"+"&nbsp;"+(rateInput)+"."+"<br/>"+
 "You will receive an amount of"+"&nbsp;"+(amount*rateInput*selectId/100)+","+"<br/>"+
 "in the year"+"&nbsp;"+(result2);
+}
 }
 
